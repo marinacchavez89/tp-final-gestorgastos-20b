7e3f53bce0c3f36468c9bc0c30fae79387d160ab
juancruzgauna <juancruzgauna97@gmail.com>
2024-10-26 19:29:12 -0300

    Se agrega en navbar Ver Perfil una vez el usuario logueado y se agrega pantalla de Perfil.aspx. Se incluye control html para agregar la imagen de perfil

---
+++ TpFinal-WebForms-20B-GestorGastos/Perfil.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="Perfil.aspx.cs" Inherits="TpFinal_WebForms_20B_GestorGastos.Perfil" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h2>Perfil de <%=Session["UsuarioNombre"]%></h2>
+
+    <div class="mb-3">
+        <label for="txtNombre" class="form-label">Nombre</label>
+        <asp:TextBox ID="txtNombre" CssClass="form-control" runat="server" />
+    </div>
+
+    <div class="mb-3">
+        <label for="txtEmail" class="form-label">Email</label>
+        <asp:TextBox ID="txtEmail" CssClass="form-control" runat="server" placeholder="name@example.com" TextMode="Email" />
+    </div>
+
+    <div class="mb-3">
+        <label for="txtPass" class="form-label">Contraseña</label>
+        <asp:TextBox ID="txtPass" CssClass="form-control" runat="server" TextMode="Password" />
+    </div>
+
+    <div class="mb-3">
+        <asp:Label ID="lblError" CssClass="text-danger" runat="server" Visible="false"></asp:Label>
+    </div>
+    
+    <asp:Button ID="btnModificar" Text="Modificar" CssClass="btn btn-secondary" Onclick="btnModificar_Click" runat="server" />
+
+    <div class ="mb-4"></div>
+
+    <div class="col-md-4">
+            <div class="mb-3">
+                <label class="form-label">Imagen Perfil</label>
+                <input type="file" id="txtImagen" runat="server" class="form-control" />
+            </div>
+            <asp:Image ID="imgNuevoPerfil" ImageUrl="https://www.palomacornejo.com/wp-content/uploads/2021/08/no-image.jpg"
+                runat="server" CssClass="img-fluid mb-3" />
+        </div>
+
+</asp:Content>
---
+++ TpFinal-WebForms-20B-GestorGastos/Perfil.aspx.designer.cs
@@ -0,0 +1,80 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace TpFinal_WebForms_20B_GestorGastos
+{
+
+
+    public partial class Perfil
+    {
+
+        /// <summary>
+        /// Control txtNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombre;
+
+        /// <summary>
+        /// Control txtEmail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
+
+        /// <summary>
+        /// Control txtPass.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPass;
+
+        /// <summary>
+        /// Control lblError.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblError;
+
+        /// <summary>
+        /// Control btnModificar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnModificar;
+
+        /// <summary>
+        /// Control txtImagen.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputFile txtImagen;
+
+        /// <summary>
+        /// Control imgNuevoPerfil.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image imgNuevoPerfil;
+    }
+}
