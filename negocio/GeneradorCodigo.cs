1ea299aa0c216c48eca8726785f2f4d2d7d81822
martin lozano <martin.lozano995@gmail.com>
2024-11-03 10:35:58 -0300

    Correccion del archivo que no se muestra

--- negocio/GeneradorCodigo.cs
+++ negocio/GeneradorCodigo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,37 @@ namespace negocio
 {
     internal class GeneradorCodigo
     {
+        private bool VerificarCodigo(string codigo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("SELECT COUNT(*) FROM Grupos WHERE codigoInvitacion = @codigo");
+                datos.setearParametro("@codigo", codigo);
+                int count = (int)datos.ejecutarScalar();
+                if (count > 0)
+                { return true; }
+                else { return false; }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+        private string GenerarCodigo()
+        {
+                Random random = new Random();
+                string codigo = "";
+            for (int i = 0; i < 6; i++)
+            {
+                int digito = random.Next(0, 9);
+                codigo += (char)digito;
+            }
+        }
     }
 }
