0b5884e310fc967eee710067b8f0016a071e3ce0
Marina Celeste Chavez <94614751+marinacchavez89@users.noreply.github.com>
2024-11-12 14:41:09 -0300

    Se corrige texto plano de envío de emails aclarando como proceder ante una inivtación.

--- negocio/EmailService.cs
+++ negocio/EmailService.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Mail;
+using System.ComponentModel;
 
 namespace negocio
 {
@@ -27,7 +28,8 @@ namespace negocio
             email.To.Add(emailDestino);
             email.Subject = asunto;
             email.IsBodyHtml = true;
-            email.Body = "<h1>¡Te unieron a un grupo para compartir gastos!</h1> <br> Iniciá sesión con el email " + emailRegistro + " y usa como contraseña el codigo de invitación: " + codInvitacion;
+            email.Body = "<h1>¡Te unieron a un grupo para compartir gastos!</h1> <br> Si aun no estas registrado o no has cambiado la contraseña, iniciá sesión con el email " + emailRegistro + " y usa como contraseña el código de invitación: " + codInvitacion +
+                "<br> Si ya te habías unido a un grupo y no recordas el código de invitación ingresá a 'Olvide mi contraseña' al iniciar sesión." ;
         }
 
         public void enviarEmail()
@@ -53,8 +55,9 @@ namespace negocio
                 mensaje.To.Add(new MailAddress(emailParticipante));
                 mensaje.Subject = "¡Te unieron a un grupo de gastos!";
                 mensaje.Body = $"<h1>¡Hola, {emailParticipante}!</h1>" +
-                               $"<p> Te invitaron a que te unas a un grupo de gestión de gastos. Podes ingresar con el email {emailParticipante} y el código de invitacion {codInvitacionPass} como contraseña y participar en la división de gastos con tus amigos.</p>" +
-                               $"<p> Te esperamos pronto en la app, no olvides cambiar tu password. ¡Gracias por elegirnos!</p>" +
+                               $"<p> Te invitaron a que te unas a un grupo de gestión de gastos. Si nunca te has registrado ni cambiado la contraseña, podes ingresar con el email {emailParticipante} y el código de invitacion {codInvitacionPass} como contraseña y participar en la división de gastos con tus amigos.</p>" +
+                               $"<p> Si ya te habías unido a un grupo, nunca cambiaste la contraseña y no recordás el primer código de inivitación, podes elegir desde Iniciar Sesión 'Olvidé mi contraseña' y generar una nueva.</p>" +
+                               $"<p> Te esperamos pronto en la app, no olvides cambiar tu password si sigue siendo el código de invitación. ¡Gracias por elegirnos!</p>" +
                                $"<br/>" +
                                $"<p>Atentamente,</p>" +
                                $"<p>App Gestor de Gastos TweentyB</p>";
